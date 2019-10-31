a5b9835cf5e5bf0fdd062377c7f681e438ffaf49
andres1147 <andresgarcesc1147@gmail.com>
2019-10-31 10:56:44 -0500

    ejercicio Angular de nuevo

--- ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/mostrar-nombre/mostrar-nombre.component.ts
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/mostrar-nombre/mostrar-nombre.component.ts
@@ -1,20 +1,18 @@
-import { Component, OnInit} from '@angular/core';
-
+import { Component} from '@angular/core';
+/**
+ * @description La clase mostrarNombre permite imprimir en pantalla mis datos personales
+ * @author Andres Garces
+ */
 @Component({
   selector: 'app-mostrar-nombre',
   templateUrl: './mostrar-nombre.component.html',
   styleUrls: ['./mostrar-nombre.component.css']
 })
 
-export class MostrarNombreComponent implements OnInit {
+export class MostrarNombreComponent {
 
   nombre= "Andrés Garcés Candamil";
   ciudad = "Manizales";
   repositorio = "https://github.com/andres1147/gitAndresGarces.git";
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
