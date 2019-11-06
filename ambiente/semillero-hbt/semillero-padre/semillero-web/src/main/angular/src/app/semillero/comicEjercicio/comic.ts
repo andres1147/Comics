3bd8ce76e38a02e70cf863f7b84185f9a243608d
andres1147 <andresgarcesc1147@gmail.com>
2019-11-06 17:25:07 -0500

    Ejercicio Angular sesion 2 (esta en mostrar-nombre)

---
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/comicEjercicio/comic.ts
@@ -0,0 +1,60 @@
+/**
+ * @description Clase Comic que contiene la informacion de un comic
+ * 
+ * @author Andrés Garcés <andresgarcesc1147@gmail.com>
+ */
+export class Comic {
+
+    /**
+    * Indicador de resultado.
+    */
+    public id: number;
+
+    /**
+    * Indicador de resultado.
+    */
+    public nombre: string;
+
+    /**
+    * Indicador de resultado.
+    */
+    public editorial: string;
+
+    /**
+    * Indicador de resultado.
+    */
+    public tematica: string;
+
+    /**
+   * Indicador de resultado.
+   */
+    public numeroPaginas: number;
+
+    /**
+    * Indicador de resultado.
+    */
+    public precio: string;
+
+    /**
+  * Indicador de resultado.
+  */
+    public autores: string;
+
+    /**
+   * Indicador de resultado.
+   */
+    public aColor: boolean;
+
+    /**
+  * Indicador de resultado.
+  */
+    public fechaVenta: Date;
+
+    /**
+    * Indicador de resultado.
+    */
+    public estado: string;
+
+    
+
+}
