3333a7298f37a58f75b8d7ed61f262277ebccb25
andres1147 <andresgarcesc1147@gmail.com>
2019-11-12 23:31:20 -0500

    actualizacion

--- ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/app.module.ts
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule} from '@angular/http';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
 import { APP_BASE_HREF } from '@angular/common';
-import { GestionarComicComponent } from './semillero/componentes/gestionarComic/gestionar-comic';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
 import { MenuComponent } from './semillero/componentes/menu/menu-component';
 import { BienvenidaComponent } from './semillero/componentes/home/bienvenida-component';
-import { MostrarNombreComponent } from './semillero/componentes/mostrar-nombre/mostrar-nombre.component';
+import { CrearPersonaComponent } from './semillero/componentes/crearPersona/crear-persona-component';
+import { GestionarComicComponent } from './semillero/componentes/gestionarComic/gestionar-comic';
 
 // DTOs
 export { ComicDTO } from './semillero/dto/comic.dto';
@@ -21,16 +22,16 @@ export { ResultadoDTO } from './semillero/dto/resultado.dto';
     AppComponent,
     MenuComponent,
     BienvenidaComponent,
-    GestionarComicComponent,
-    MostrarNombreComponent,
+    CrearPersonaComponent,
+    GestionarComicComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpModule,
     HttpClientModule,
-    FormsModule
-    
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
   	{ provide: APP_BASE_HREF, useValue: '/SemilleroHBT' }
